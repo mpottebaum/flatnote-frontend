3b7f19e87624eb00c59c804344551d3e7d8a182c
mpottebaum <mpottebaum@gmail.com>
2020-05-04 11:13:03 -0500

    setup login form and fetch

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,44 @@
 import React from 'react'
+import { users } from '../urlPaths'
 
-const Login = props => {
-    return (
-        <div>
+class Login extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            username: ''
+        }
+    }
 
-        </div>
-    )
+    handleChange = e => {
+        this.setState({
+            username: e.target.value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        const configObj = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(this.state)
+        }
+        fetch(users, configObj)
+            .then(resp => resp.json())
+            .then(user => {
+                console.log(user)
+            })
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit} >
+                <input onChange={this.handleChange} type='text' name='username' value={this.state.username} />
+                <input type='submit' value='login' />
+            </form>
+        )
+    }
 }
 
 export default Login
---
+++ src/urlPaths.js
@@ -0,0 +1,3 @@
+const DOMAIN = 'http://localhost:3000'
+
+export const users = `${DOMAIN}/users`
