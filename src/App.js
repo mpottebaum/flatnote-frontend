96ab1098455d623b15d4b1748ef3a12ed56d2810
mpottebaum <mpottebaum@gmail.com>
2020-05-04 10:46:36 -0500

    setup Login component and route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom'
+import Login from './components/Login'
 import './App.css';
 
 function App() {
   return (
     <Router>
-      
+      <Route exact path='/login' component={Login} />
     </Router>
   );
 }
