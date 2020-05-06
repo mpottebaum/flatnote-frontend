cdb289dc4290e02a458c755d7c24bc77966b564b
mpottebaum <mpottebaum@gmail.com>
2020-05-06 17:00:56 -0500

    styled notes as cards

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import ListGroup from 'react-bootstrap/ListGroup'
+import Card from 'react-bootstrap/Card'
 
 const Note = props => {
     const { id, title, content} = props.note
     return <ListGroup.Item onClick={e => props.handleNoteClick(id)}>
-        <p>{title}</p>
-        <p>{content}</p>
+        <Card>
+            <Card.Header>
+                {title}
+            </Card.Header>
+            <Card.Body>
+            {content}
+            </Card.Body>
+        </Card>
     </ListGroup.Item>
 }
 
--- src/components/ShowNote.js
+++ src/components/ShowNote.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
 
 const ShowNote = props => {
     const tagNames = props.note.tags.map(tag => tag.name).join(', ')
-    return <div>
-            <h1>{props.note.title}</h1>
-            <p>{props.note.content}</p>
-            <p>Tags: {tagNames}</p>
+    return <Card>
+        <Card.Body>
+            <Card.Title>{props.note.title}</Card.Title>
+            <Card.Text>{props.note.content}</Card.Text>
+            <Card.Text>Tags: {tagNames}</Card.Text>
+            <Card.Text className='mb-2 text-muted'>Created: {props.note.created_at}</Card.Text>
+            <Card.Text className='mb-2 text-muted'>Last updated: {props.note.updated_at}</Card.Text>
             <Button onClick={() => props.handleDeleteNote(props.note.id)}>Delete</Button>
             <Button onClick={props.toggleEditing}>Edit</Button>
-        </div>
+        </Card.Body>
+        </Card>
 }
 
 export default ShowNote
